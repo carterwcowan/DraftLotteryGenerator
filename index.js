5c4ba071f90417b5a528ddecff9d832820f033dd
carterwcowan <carterwcowan@gmail.com>
2018-08-20 18:05:22 -0500

    update output to be more readable, add json standings

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
-let ticketArray = [8, 8, 8, 8, 8, 8, 8, 8, 7, 7, 7, 7, 7, 7, 7, 6, 6, 6, 6, 6, 6, 5, 5, 5, 5, 5, 4, 4, 4, 4, 3, 3, 3, 2, 2, 1];
-let picksLeft = 8;
+
+let userList = require('./standings');
+
+function createTickets(userList) {
+    let ticketList = [];
+    userList.forEach(element => {
+        for (var i = 0; i <= userList.length - element.standing; i++) {
+            ticketList.push(element.player);
+        }
+    });
+    return ticketList;
+}
 
 function run() {
-    console.log('--- INITIAL LOTTERY ---');
-    console.log(ticketArray);
+    const draftOrder = getDraftOrder(userList.standings);
+    console.log(draftOrder);
+}
 
+function getDraftOrder(userList) {
+    let ticketList = createTickets(userList);
+    let picksRemaining = userList.length;
+    let draftOrder = [];
 
-    while (picksLeft > 0) {
-        console.log('number of tickets left:', ticketArray.length);
-        console.log('tickets:', ticketArray);
-
-        let pick = makePick(ticketArray);
-        console.log('Pick:', pick);
-        ticketArray = removeTickets(ticketArray, pick);
-        picksLeft--;
+    for (var i = picksRemaining; i > 0; i--) {
+        let pick = makePick(ticketList);
+        draftOrder.push(pick);
+        picksRemaining--;
+        ticketList = ticketList.filter(player => player != pick);
     }
 
+    return draftOrder;
 }
 
 function makePick(ticketArray) {
     return ticketArray[Math.floor(Math.random() * ticketArray.length)]
 }
-
-function removeTickets(ticketArray, ticketToRemove) {
-    return ticketArray.filter(item => item != ticketToRemove);
-}
-
 
 run();
